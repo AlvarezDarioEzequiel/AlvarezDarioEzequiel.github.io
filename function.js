4fbe3ceeb06103d8cab965fa669c8feb1cd9763c
AlvarezDarioEzequiel <93893084+AlvarezDarioEzequiel@users.noreply.github.com>
2023-03-08 05:08:48 -0300

    Cambios al 99.6%

--- function.js
+++ function.js
@@ -1,4 +1,33 @@
-const collection = document.getElementsByClassName("lista");
-for (let i = 0; i < collection.length; i++) {
-  collection[i].style.backgroundColor = "red";
+//title page
+let previousTitle = document.title;
+
+window.addEventListener("blur", () => {
+  previousTitle = document.title;
+  document.title = "More About Darío"
+})
+
+window.addEventListener("focus", () => {
+  document.title = previousTitle;
+})
+
+
+//footer message efect
+let text = document.getElementById("thanks");
+let str = text.innerHTML;
+
+text.innerHTML = "";
+
+let speed = 200;
+let i=0;
+
+//function
+
+function typeWriter(){
+  if(i<str.length){
+    text.innerHTML += str.charAt(i);
+    i++;
+    setTimeout(typeWriter,speed);
+  }
 }
+
+setTimeout(typeWriter, speed);
