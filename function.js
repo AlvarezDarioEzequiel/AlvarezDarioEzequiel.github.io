a10791712028ac2bb7d89d66e3fb5d0dc0a35f4e
AlvarezDarioEzequiel <93893084+AlvarezDarioEzequiel@users.noreply.github.com>
2024-12-24 13:46:28 -0300

    Cambios al 99.9988%

--- function.js
+++ function.js
@@ -1,16 +1,35 @@
+//scroll-up
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+  var CurrentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (CurrentScroll > 0){
+      window.scrollTo(0,0);
+  }
+}
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function (){
+  var scroll = document.documentElement.scrollTop;
+
+  if (scroll > 1800){
+    buttonUp.style.transform = "scale(1)";
+  }else if (scroll < 1800){
+    buttonUp.style.transform = "scale(0)";
+  }
+}
+
 //title page
 let previousTitle = document.title;
 
 window.addEventListener("blur", () => {
   previousTitle = document.title;
-  document.title = "More About Dario"
+  document.title = "Regresa para saber más"
 })
 
 window.addEventListener("focus", () => {
   document.title = previousTitle;
 })
-
-
 //footer message effect
 let text = document.getElementById("thanks");
 let str = text.innerHTML;
